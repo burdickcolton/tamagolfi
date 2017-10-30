0c7ea7c48d7c130fd82d488d156ab07756f2729c
InvestigatorMelodytchi <31948486+InvestigatorMelodytchi@users.noreply.github.com>
2017-10-30 01:25:12 -0400

    Add files via upload

--- js/interval.js
+++ js/interval.js
@@ -5,4 +5,4 @@
 /*  http://javascriptobfuscator.com                                        */
 /*                                                                         */
 /***************************************************************************/
-var _$_c4ee=["\x41\x63\x74","\x63\x6C\x65\x61\x72","\x44\x72\x61\x77"];function gameInterval(){drawInterval();if(objTransition!= undefined){objTransition[_$_c4ee[0]]()};if(mouseState== 2){mouseState= 1};if(menuBackTick> 0){menuBackTick--}else {menuBackTick= 3;if(menuBackScroll>  -24){menuBackScroll--}else {menuBackScroll+= 23}}}function drawInterval(){env[_$_c4ee[1]]();objControl[_$_c4ee[2]]();if(objTransition!= undefined){objTransition[_$_c4ee[2]]()}}
+var _$_5ccd=["\x41\x63\x74","\x63\x6C\x65\x61\x72","\x44\x72\x61\x77"];function gameInterval(){drawInterval();if(objTransition!= undefined){objTransition[_$_5ccd[0]]()};if(mouseState== 2){mouseState= 1};if(menuBackTick> 0){menuBackTick--}else {menuBackTick= 3;if(menuBackScroll>  -24){menuBackScroll--}else {menuBackScroll+= 23}}}function drawInterval(){env[_$_5ccd[1]]();objControl[_$_5ccd[2]]();if(objTransition!= undefined){objTransition[_$_5ccd[2]]()}}
